27d5f82345105b9754426cf8f9ac601f1698ec5e
carolainseija <carolainsilva1@gmail.com>
2024-07-28 12:27:25 -0300

    email

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,36 @@
-// src/components/Modal.js
-import React from 'react';
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 import './Modal.css';
 
 const Modal = ({ isOpen, closeModal }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
   if (!isOpen) return null;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Aquí puedes implementar la lógica para enviar el formulario
-    // Puedes agregar la lógica para guardar los datos o enviarlos a través de una API, por ejemplo
-    // Una vez enviado, podrías cerrar el modal
-    closeModal();
+
+    emailjs.sendForm('service_an7fqn8', 'template_3f8r48e', e.target, 'aCwG3r1EsKwLz0F1x')
+      .then((result) => {
+        console.log(result.text);
+        alert("Correo enviado con éxito");
+        closeModal();
+      }, (error) => {
+        console.log(error.text);
+        alert("Hubo un error al enviar el correo");
+      });
   };
 
   return (
@@ -23,15 +43,36 @@ const Modal = ({ isOpen, closeModal }) => {
         <form onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="name">Tu Nombre</label>
-            <input type="text" id="name" name="name" required />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
           </div>
           <div className="form-group">
             <label htmlFor="email">Tu Correo</label>
-            <input type="email" id="email" name="email" required />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
           </div>
           <div className="form-group">
             <label htmlFor="message">Mensaje</label>
-            <textarea id="message" name="message" rows="4" required />
+            <textarea
+              id="message"
+              name="message"
+              rows="4"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
           </div>
           <button type="submit">Enviar</button>
         </form>
